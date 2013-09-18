f92b6eafca6edf4a7c749c6d9019caa11a5eb8e5
Andrew Fitz Gibbon <gibbon@amazon.com>
2013-09-18 12:33:53 -0700

    Remove reference to samples_config which doesn't exist anymore.

--- s3_sample.rb
+++ s3_sample.rb
@@ -13,7 +13,6 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-#require File.expand_path(File.dirname(__FILE__) + '/samples_config')
 require 'rubygems'
 require 'bundler/setup'
 require 'aws-sdk'
