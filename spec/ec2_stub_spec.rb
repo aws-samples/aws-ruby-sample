8a160c2a2e04846b82eb8e4c4799c7cbe9ef62b5
Josh Kline <github.com@track.jonfram.net>
2015-04-30 17:16:04 -0700

    Showing incorrect return from stubbed resource
    A very simple RSpec example showing how stub_responses affects the
    Aws::EC2::Client interface correctly, but results in doubled resonses from the
    Aws::EC2::Resource interface.

---
+++ spec/ec2_stub_spec.rb
@@ -0,0 +1,28 @@
+require "aws-sdk"
+
+RSpec.describe Aws::EC2 do
+  let(:client) { Aws::EC2::Client.new(:region => 'us-east-1', :stub_responses => true) }
+  let(:resource) { Aws::EC2::Resource.new(:client => client) }
+
+  context "when describe_instances is stubbed to return a single instance" do
+    before do
+      client.stub_responses(
+        :describe_instances, {
+          :reservations => [{:instances=>[{:instance_id => "i-1", :state => {:name => "running"}, :tags =>[{:key => "Name", :value => "example-1" }]}]}]
+        }
+      )
+
+    end
+    context Aws::EC2::Client do
+      it "#describe_instances to return 1 instance" do
+        expect(client.describe_instances.reservations.first.instances.size).to eq 1
+      end
+    end
+
+    context Aws::EC2::Resource do
+      it "#instances to return 1 instance" do
+        expect(resource.instances.to_a.size).to eq 1
+      end
+    end
+  end
+end
